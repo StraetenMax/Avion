9a9e906bf35a2fd64473b82ba5f78213aac35b5d
StraetenMax <maxstraeten@yahoo.com>
2017-05-17 18:38:16 +0200

    classe avion propriétés constructeur

--- src/constructionAvion/Avion.java
+++ src/constructionAvion/Avion.java
@@ -1,5 +1,26 @@
 package constructionAvion;
 
 public class Avion {
-
+	//propriétés
+	private int id;
+	private String marque;
+	private double vitesse;
+	private int puissance;
+	public static int count;
+	
+	//constructeur vide initialisé
+	public Avion(){
+		id = ++count;
+		marque = "";
+		vitesse = 00.00;
+		puissance = 0;	
+	}
+	public Avion(String marque, double vitesse, int puissance){
+		id = ++count;
+		this.marque = marque;
+		this.vitesse = vitesse;
+		this.puissance = puissance;
+	}
+	
+	//
 }
